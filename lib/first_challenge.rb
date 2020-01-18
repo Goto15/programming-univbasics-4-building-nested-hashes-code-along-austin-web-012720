1e9ed0d1705616d1699b82f44713c4bda00d4d22
Benjamin Sullivan <benjamin.r.sullivan@gmail.com>
2020-01-18 13:46:34 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -12,14 +12,24 @@ def first_challenge
        :hero => {
          :name => "Romeo",
          :age => 15,
-         :status => "alive
+         :status => "alive"
        },
        :hero_friends => []
      },
      :capulet => {
-       :patriarch => {},
-       :matriarch => {},
-       :heroine => {},
+       :patriarch => {
+         :name => "Lord Capulet",
+         :age => 50
+       },
+       :matriarch => {
+         :name => "Lady Capulet",
+         :age => 51
+       },
+       :heroine => {
+         :name => "Juliet",
+         :age => 15,
+         :status => "alive"
+       },
        :heroine_friends => []
      }
    }
